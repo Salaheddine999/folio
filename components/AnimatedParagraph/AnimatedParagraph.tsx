72e5a1b6f97129db85770245b1143d4dbdd34809
Salah eddine Ab <abdelwahid.salaheddine@gmail.com>
2023-04-01 22:55:06 +0000

    remove resume button

--- components/AnimatedParagraph/AnimatedParagraph.tsx
+++ components/AnimatedParagraph/AnimatedParagraph.tsx
@@ -24,7 +24,7 @@ export default function AnimatedParagraph() {
             </RoughNotation>
           , and I&apos;m a frontend developer passionate about open source and technology that has a positive impact on people&apos;s lives. I enjoy creating beautiful and user-friendly web interfaces by constantly exploring and experimenting with new tools and techniques. In my spare time, I go out with friends or read blogs and articles from experts to stay up-to-date with the latest trends in the industry.
       </p>
-      <Resume/>
+      {/* <Resume/> */}
     </>
   );
 }
