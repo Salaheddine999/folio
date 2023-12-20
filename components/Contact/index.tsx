ca10240077f7dffab8a449ee5fc01e49bb319dbd
Salah eddine Ab <abdelwahid.salaheddine@gmail.com>
2023-12-20 04:28:51 +0100

    changes

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import styles from './Contact.module.scss'
+import { RoughNotation } from "react-rough-notation";
 
 export default function Contact() {
   const [copiedLink, setCopiedLink] = useState(false)
@@ -11,17 +12,36 @@ export default function Contact() {
   }
   
   return (
-    <div className={styles.container}>
-        <div className={styles.loop_holder}>
-          <div className={styles.loop_holder__text}>GET IN TOUCH<span className={styles.spaner}>•</span>GET IN TOUCH<span className={styles.spaner}>•</span>GET IN TOUCH<span className={styles.spaner}>•</span></div>
-          <div className={styles.loop_holder__text}>GET IN TOUCH<span className={styles.spaner}>•</span>GET IN TOUCH<span className={styles.spaner}>•</span>GET IN TOUCH<span className={styles.spaner}>•</span></div>
-          <div className={styles.loop_holder__text}>GET IN TOUCH<span className={styles.spaner}>•</span>GET IN TOUCH<span className={styles.spaner}>•</span>GET IN TOUCH<span className={styles.spaner}>•</span></div>
-        </div>
-        {!copiedLink ? (
-          <button className={styles.copy_email} onClick={onCopy}>COPY EMAIL</button>
-        ):(
-          <button className={styles.copy_email}>EMAIL COPIED!</button>
-        )}
-    </div>
+    <>
+      
+      <div className={styles.container}>
+          <h2>
+          <RoughNotation
+              type="highlight"
+              color="#D7FE52"
+              multiline
+              show
+              animationDelay={3800}
+              padding={[ 2, 14, 2, 14 ]}
+              strokeWidth={4}
+            >
+            Always up for meeting awesome new people!
+            </RoughNotation>
+          </h2>
+          <div className={styles.animation_container}>
+            <div className={styles.loop_holder}>
+              <div className={styles.loop_holder__text}>Contact me<span className={styles.spaner}>•</span>Contact me<span className={styles.spaner}>•</span>Contact me<span className={styles.spaner}>•</span></div>
+              <div className={styles.loop_holder__text}>Contact me<span className={styles.spaner}>•</span>Contact me<span className={styles.spaner}>•</span>Contact me<span className={styles.spaner}>•</span></div>
+              <div className={styles.loop_holder__text}>Contact me<span className={styles.spaner}>•</span>Contact me<span className={styles.spaner}>•</span>Contact me<span className={styles.spaner}>•</span></div>
+            </div>
+            {!copiedLink ? (
+              <button className={styles.copy_email} onClick={onCopy}>CLICK TO COPY EMAIL</button>
+            ):(
+              <button className={styles.copy_email}>EMAIL COPIED!</button>
+            )}
+          </div>
+
+      </div>
+    </>
   )
 }
